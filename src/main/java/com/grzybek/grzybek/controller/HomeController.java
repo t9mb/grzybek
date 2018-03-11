5a2b397cddf3ea97957686e6d4d498a453d238d8
t9mb <32464601+miccom91@users.noreply.github.com>
2018-03-11 14:57:12 +0100

    edit templates add error handeling edit main

---
+++ src/main/java/com/grzybek/grzybek/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.grzybek.grzybek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String getHome(Model model){
+        model.addAttribute("nameapp", "grzybek");
+        model.addAttribute("deskriptionapp", "Aplikacja, która umożliwia lokalizowanie w polskich lasach miejsc występowania grzybów ");
+        return "pages/index";
+    }
+
+    @GetMapping("/errors")
+    public String error(){
+        return "pages/error";
+        }
+    }
+
