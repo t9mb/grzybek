15c52a74dbeb0156b1f624f07bc958b6effd4cb8
PatrykSzereluk <patziomekk@gmail.com>
2018-03-20 17:45:42 +0100

    edit attributesController

--- src/main/java/com/grzybek/grzybek/controller/AttributeController.java
+++ src/main/java/com/grzybek/grzybek/controller/AttributeController.java
@@ -1,16 +1,32 @@
 package com.grzybek.grzybek.controller;
 
 
-import lombok.AllArgsConstructor;
+import com.grzybek.grzybek.services.AttributeService;
+import lombok.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/attribute")
 @AllArgsConstructor
-
 public class AttributeController {
 
+    private final AttributeService attributeService;
+
+    @GetMapping
+    public String getAllUser(Model model) {
+        model.addAttribute("attributelist", attributeService.getListAllAttributes());
+        return "pages/attributelist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return "redirect:/user";
+    }
 
 
 }
