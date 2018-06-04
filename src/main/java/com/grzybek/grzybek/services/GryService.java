b2fe3f4ddd13cf8cc9fa6f81d7406e9ce2b21bdb
fabros <fabros@op.pl>
2018-06-05 01:37:39 +0200

    jakieś nocne zmiany
    afafg

---
+++ src/main/java/com/grzybek/grzybek/services/GryService.java
@@ -0,0 +1,11 @@
+package com.grzybek.grzybek.services;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GryService {
+
+}
